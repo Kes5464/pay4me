f600a26291db042e2068302c2c779197eefb558b
Kestine Chukwuemeka <kestine.chukwuemeka@gmail.com>
2025-11-13 16:12:47 +0100

    Keep all settings configured for pay4me.com.ng domain

--- api/health.js
+++ api/health.js
@@ -1,6 +1,6 @@
 // Health Check API - Test if backend is working
 export default function handler(req, res) {
-    res.setHeader('Access-Control-Allow-Origin', 'https://pay4me.com.ng');
+    res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'GET, OPTIONS');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
 
